3823a5a5aa4cb991970f482f3bf4a27d03fb7ca2
Rieyu <21bd1a0505csea@gmail.com>
2024-04-26 19:23:20 +0530

    Added base bot

---
+++ responses.py
@@ -0,0 +1,10 @@
+
+
+def handle_response(message) ->str:
+    p_message = message.lower()
+
+    if(p_message=='hello'):
+        return "under maintainenance"
+    
+    
+    return "under maintainenance"
